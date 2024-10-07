ca693cef909e4e3aeb227213ae247452af7c1e5c
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-10-07 11:26:24 -0400

    add: db image storage

--- Migrations/20241007150926_DocumentImage.Designer.cs
+++ Migrations/20241007150926_DocumentImage.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IdVaultServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace idvault_server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241007150926_DocumentImage")]
+    partial class DocumentImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
